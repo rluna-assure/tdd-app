77989f8ce75e773918c5eda3aeeb2597e12dd57b
Ronald Luna <ronald.luna@bolivia.assuresoft.com>
2026-05-29 15:53:44 -0400

    add tests for number 3 and 4

--- project/project.Core/RomanConverter.cs
+++ project/project.Core/RomanConverter.cs
@@ -4,10 +4,21 @@ public class RomanConverter
 {
     public string ConvertToRoman(int number)
     {
+
         if (number == 1)
         {
             return "I";
         }
+        if (number == 3)
+        {
+            return "III";
+        }
+        if (number == 4)
+        {
+            return "IV";
+        }
+
+
         throw new NotImplementedException();
     }
 }
