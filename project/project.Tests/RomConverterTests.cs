1ef2b653996cbc9c3216b35c29d762afe33ec0d7
Ronald Luna <ronald.luna@bolivia.assuresoft.com>
2026-05-29 15:45:50 -0400

    test for number 3 added

--- project/project.Tests/RomConverterTests.cs
+++ project/project.Tests/RomConverterTests.cs
@@ -18,6 +18,19 @@ public class RomanConverterTests
     }
 
     [Fact]
+    public void ConvertToRoman_WhenNumberIs3_ReturnsIII()
+    {
+        // Arrange
+        var converter = new RomanConverter();
+
+        // Act
+        var result = converter.ConvertToRoman(3);
+
+        // Assert
+        Assert.Equal("III", result);
+    }
+
+    [Fact]
     public void ConvertToRoman_WhenNumberIs4_ReturnsIV()
     {
         // Arrange
