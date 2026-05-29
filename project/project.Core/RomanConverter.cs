b37610ef6488327197cc175dae7ffa9a67c07c9a
Ronald Luna <ronald.luna@bolivia.assuresoft.com>
2026-05-29 16:33:16 -0400

    refactor function convertRoman for values until 1000

--- project/project.Core/RomanConverter.cs
+++ project/project.Core/RomanConverter.cs
@@ -7,6 +7,14 @@ public class RomanConverter
 
     new Dictionary<int, string> keyValuePairs = new Dictionary<int, string>
     {
+        {1000, "M"},
+        {900, "CM"}, 
+        {500, "D"}, 
+        {400, "CD"}, 
+        {100, "C"},  
+        {90, "XC"},  
+        {50, "L"},  
+        {40, "XL"},
         {10, "X"},
         {9, "IX"},
         {5, "V"},
